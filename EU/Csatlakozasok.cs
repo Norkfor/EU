727090acefe127d9a52bbfe5e2723dcbdafbdc2b
Norkfor <norkfor@gmail.com>
2021-09-20 08:27:24 +0200

    MasodikFeladat létrehozása (adatok beolvasása)

---
+++ EU/Csatlakozasok.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EU
+{
+  class Csatlakozasok
+  {
+
+    private string Orszag;
+
+    public string orszag
+    {
+      get { return Orszag; }
+      set { Orszag = value; }
+    }
+
+    private DateTime Ev;
+
+
+
+    public DateTime ev
+    {
+      get { return Ev; }
+      set { Ev = value; }
+    }
+
+    public Csatlakozasok(string orszag, DateTime ev)
+    {
+      Orszag = orszag;
+      Ev = ev;
+    }
+
+  }
+}
---
+++ EU/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EU
+{
+  class Program
+  {
+    static List<Csatlakozasok> csatlakozas = new List<Csatlakozasok>();
+    static void Main(string[] args)
+    {
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      HetedikFeladat();
+      NyolcadikFeladat();
+      Console.ReadKey();
+    }
+
+
+    private static void NyolcadikFeladat()
+    {
+
+    }
+
+    private static void HetedikFeladat()
+    {
+
+    }
+
+    private static void HatodikFeladat()
+    {
+
+    }
+
+    private static void OtodikFeladat()
+    {
+
+    }
+
+    private static void NegyedikFeladat()
+    {
+
+    }
+
+    private static void HarmadikFeladat()
+    {
+
+    }
+
+    private static void MasodikFeladat()
+    {
+      Console.WriteLine("2. feladat: adatok beolvasása");
+      StreamReader be = new StreamReader("EUcsatlakozas.txt");
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split(';');
+        csatlakozas.Add(new Csatlakozasok(
+          a[0], Convert.ToDateTime(a[1])));
+      }
+      be.Close();
+    }
+  }
+}
+
